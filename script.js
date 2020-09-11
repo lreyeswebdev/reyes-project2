a13f7502456b3e5d1880592e1c2c5b11999581d3
Ligaya Reyes <l.reyes062018@gmail.com>
2020-09-11 20:42:00 +0800

    included game winning conditions and winner display

--- script.js
+++ script.js
@@ -8,7 +8,54 @@ let gameIsLive = true;
 let xIsNext = true;
 let winner = null;
 
+// functions
 
+const winCondition = (letter) => {
+    gameIsLive = false;
+    winner = letter;
+    displayContent.innerHTML = `${winner} has won! 🏆`;
+};
+
+const checkGameStatus = () => {
+    const one = squares[0].classList[2];
+    const two = squares[1].classList[2];
+    const three = squares[2].classList[2];
+    const four = squares[3].classList[2];
+    const five = squares[4].classList[2];
+    const six = squares[5].classList[2];
+    const seven = squares[6].classList[2];
+    const eight = squares[7].classList[2];
+    const nine = squares[8].classList[2];
+
+    // check winner - rows, columns, diagonal
+    if (one && one === two && one === three) {        
+        winCondition(one);
+    } else if (four && four === five && four === six) {
+        winCondition(four);
+    } else if (seven && seven === eight && seven === nine) {
+        winCondition(seven);
+    } else if (one && one === four && one === seven) {
+        winCondition(one);
+    } else if (two && two === five && two === eight) {
+        winCondition(two);
+    } else if (three && three === six && three === nine) {
+        winCondition(three);
+    } else if (one && one === five && one === nine) {
+        winCondition(one);
+    } else if (three && three === five && three === seven) {
+        winCondition(three);
+    } else if (one && two && three && four && five && six && seven && eight && nine) {
+        gameIsLive = false;
+        displayContent.innerHTML = "It's a tie! 😆";
+    } else {
+        xIsNext = !xIsNext;
+        if (xIsNext) {
+            displayContent.innerHTML = "Player X's turn."
+        } else {
+            displayContent.innerHTML = "Player O's turn."
+        }
+    }
+}
 
 // event handlers
 const squareClick = (e) => {
@@ -21,12 +68,12 @@ const squareClick = (e) => {
 
     if (xIsNext) {
         classList.add('x');
-        displayContent.innerHTML = "Player O's turn.";
-        xIsNext = !xIsNext;        
+        checkGameStatus();        
+                
     } else {
         classList.add('o');
-        displayContent.innerHTML = "Player X's turn.";        
-        xIsNext = !xIsNext;
+        checkGameStatus();                
+        
     }
     
 }
